85d52ebe83067810a27b5e6b8e93023b58b064fd
Tanisha Khandelwal <tanisha.khandelwal@apisero.com>
2021-08-12 15:06:01 +0530

    2nd commit

--- Main.java
+++ Main.java
@@ -2,4 +2,6 @@
 public class Main{
 public static void main(String[] args){
 System.out.println("Hello java program");
+System.out.println("Hello java program 1");
+System.out.println("Hello java program 2");
 }}
