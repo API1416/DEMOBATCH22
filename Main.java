cff741c27ea389555a50cb53235fe5194ba4d7e9
Tanisha Khandelwal <tanisha.khandelwal@apisero.com>
2021-08-16 12:41:02 +0530

    Commit message #24

--- Main.java
+++ Main.java
@@ -6,26 +6,4 @@ System.out.println("Hello java program 1");
 System.out.println("Hello java program 2");
   System.out.println("Hello java program 3");
 
-    System.out.println("Hello java program 4");
-
-  System.out.println("Hello java program 5-copy");
-System.out.println("Hello java program-Merge");
-System.out.println("Hello java program 1-Merge");
-System.out.println("Hello java program 2-Merge");
-  System.out.println("Hello java program 3-Merge");
-    System.out.println("Hello java program 4-Merge");
-
-  System.out.println("Hello java program 5-copy-Merge");
-
- System.out.println("Hello java program");
-System.out.println("Hello java program 1");
-System.out.println("Hello java program 2");
-  System.out.println("Hello java program 3"); 
-System.out.println("Hello java program new1");
-System.out.println("Hello java program new2");
-System.out.println("Hello java program new jenkingjs");
-System.out.println("NEW PROGRAM JENKINS");
-System.out.println("Hello java program new3");
-System.out.println("abc");
-
 }}
