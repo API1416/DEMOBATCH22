abb5a095748c6b49188a9631d96edc3e8943e677
Tanisha Khandelwal <tanisha.khandelwal@apisero.com>
2021-08-16 14:11:00 +0530

    3

--- Main.java
+++ Main.java
@@ -1,13 +1,5 @@
 
 public class Main{
 public static void main(String[] args){
-System.out.println("Hello java program");
-System.out.println("Hello java program 1");
-System.out.println("Hello java program 2");
-  System.out.println("Hello java program 3");
-System.out.println("Hello java program");
-System.out.println("Hello java program 1");
-//System.out.println("Hello java program 2");
-  //System.out.println("Hello java program 3");
-
+System.out.println("Hello java program- New Jenkins");
 }}
