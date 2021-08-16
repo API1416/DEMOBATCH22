7bf6b6b5b6d013c5966eed0243c6bfdd8b5d5f62
Tanisha Khandelwal <tanisha.khandelwal@apisero.com>
2021-08-16 10:34:52 +0530

    dddd

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ System.out.println("Hello java program");
 System.out.println("Hello java program 1");
 System.out.println("Hello java program 2");
   System.out.println("Hello java program 3");
-<<<<<<< HEAD
+
     System.out.println("Hello java program 4");
 
   System.out.println("Hello java program 5-copy");
@@ -17,11 +17,9 @@ System.out.println("Hello java program 2-Merge");
 
   System.out.println("Hello java program 5-copy-Merge");
 
-=======
  System.out.println("Hello java program");
 System.out.println("Hello java program 1");
 System.out.println("Hello java program 2");
   System.out.println("Hello java program 3"); 
->>>>>>> 0a6ae258b67482b2916cf6a878b28613292bb0c8
 
 }}
